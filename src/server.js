e5f645672c38ab7e31c80cfc17ef571fe42ed277
Abdelrhman Ahmed <157153388+4abdelrhman@users.noreply.github.com>
2025-08-03 19:43:18 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    // listen for local development
     if (ENV.NODE_ENV !== 'production') {
       app.listen(ENV.PORT, () =>
         console.log('Server is up and running on PORT:', ENV.PORT)

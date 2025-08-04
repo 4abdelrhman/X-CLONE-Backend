774351e9739be45693b297b8de138eed4c887a57
Abdelrhman <4abdelrhman@gmail.com>
2025-08-04 15:44:31 +0300

    Middleware updated

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,8 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { ENV } from '../config/env.js';
+
 export const protectRoute = async (req, res, next) => {
-  if (!req.auth().isAuthenticated) {
-    return res
-      .status(401)
-      .json({ message: 'Unauthorized - u must be logged in.' });
+  if (req.auth().isAuthenticated) next();
+
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    const token = authHeader.split(' ')[1];
+    try {
+      const decoded = jwt.verify(token, ENV.JWT_SECRET);
+      req.user = { id: decoded.id };
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Unauthorized access' });
+    }
   }
-  next();
+  return res
+    .status(401)
+    .json({ message: 'Unauthorized - you must be logeed in' });
 };

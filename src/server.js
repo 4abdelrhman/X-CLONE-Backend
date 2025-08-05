d05f715d4813090bb711b537097c5468420e2871
Abdelrhman <4abdelrhman@gmail.com>
2025-08-06 01:52:07 +0300

    .

--- src/server.js
+++ src/server.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(clerkMiddleware());
 app.use(arcjetMiddleware);
 
+app.get('/', (req, res) => res.send('Hello from server'));
+
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postsRoutes);
 app.use('/api/comments', commentRoutes);
